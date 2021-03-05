fcc38252360dea6f7bfd53d87d8151cf72105634
tmicka23 <mickathp42@gmail.com>
2021-03-05 10:56:30 +0100

    checkboxes fait

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [items, setItems] = useState([
+    { label: "item1", checked: false },
+    { label: "item2", checked: true },
+    { label: "item3", checked: false },
+    { label: "item4", checked: false },
+    { label: "item5", checked: true },
+  ]);
+
+  const [allChecked, setAllChecked] = useState(false);
+
+  /**
+   * @function handleChange
+   * @param {import("react").SyntheticEvent} e
+   * @description
+   * Modifies the "checked" value individually of each checkbox according to the "onChange" event listener.
+   *
+   * if all checkboxes are checked, then update the state "allChecked" to true
+   *
+   * if at least one checkbox is not checked, then update the state "allChecked" to false
+   */
+  const handleChange = (e) => {
+    const value = e.target.id;
+    const newItems = items.map((i) => {
+      if (i.label === value) {
+        return { ...i, checked: !i.checked };
+      } else {
+        return i;
+      }
+    });
+
+    if (newItems.filter((i) => i.checked).length < newItems.length) {
+      setAllChecked(false);
+    }
+    if (newItems.filter((i) => i.checked).length === newItems.length) {
+      setAllChecked(true);
+    }
+    setItems(newItems);
+  };
+
+  /**
+   * @function handleAllChecked
+   * @param {import("react").SyntheticEvent} e
+   * @description
+   * Modifies the state of the checkbox which manages the selection / deselection of all the checkboxes in the list
+   *
+   * then iterates over the list of checkboxes and modifies the "checked" value of all the checkboxes in the list according to the event
+   */
+  const handleAllChecked = (e) => {
+    const { checked } = e.target;
+
+    const newItems = items.map((i) => ({ ...i, checked }));
+
+    setAllChecked(!allChecked);
+    setItems(newItems);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CheckBox</h1>
+      <label htmlFor="allItems">
+        {allChecked ? "Deselect all" : "Select all"}
+      </label>
+      <input
+        type="checkbox"
+        checked={allChecked}
+        onChange={handleAllChecked}
+        id="allItems"
+      />
+
+      <ul>
+        {items?.map((item, index) => (
+          <li key={`${item?.label}-${index}`}>
+            <label htmlFor={item?.label}>{item?.label}</label>
+            <input
+              type="checkbox"
+              id={item?.label}
+              checked={item?.checked}
+              onChange={handleChange}
+            />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
